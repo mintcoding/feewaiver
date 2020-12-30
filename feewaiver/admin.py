27cbf430e74634965095b7891f4f59c75c27f108
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2020-12-30 10:47:24 +0800

    Add GlobalSettings admin form

--- feewaiver/admin.py
+++ feewaiver/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from feewaiver.models import *
+from feewaiver.main_models import GlobalSettings
 
 @admin.register(FeeWaiver)
 class FeeWaiverAdmin(admin.ModelAdmin):
@@ -62,4 +63,29 @@ class ApproversGroupAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False 
+
+
+@admin.register(GlobalSettings)
+class GlobalSettingsAdmin(admin.ModelAdmin):
+    def get_fields(self, request, obj=None):
+        if obj.key == GlobalSettings.KEY_FEEWAIVER_TEMPLATE_FILE:
+            return ['key', '_file',]
+        else:
+            return ['key', 'value',]
+
+    def get_readonly_fields(self, request, obj=None):
+        return ['key',]
+
+    #def get_form(self, request, obj=None, **kwargs):
+    #    form = super(GlobalSettingsAdmin, self).get_form(request, obj, **kwargs)
+    #    if obj.key == GlobalSettings.KEY_APIARY_SITES_LIST_TOKEN:
+    #        link_to = '/api/apiary_site/export/?' + ApiaryGlobalSettings.KEY_APIARY_SITES_LIST_TOKEN + '=' + obj.value
+    #        http_host = request.META['HTTP_HOST']
+    #        display_link_to = http_host + link_to
+    #        form.base_fields['value'].help_text = '<a href="' + link_to + '">' + display_link_to + '</a>'
+    #    return form
+
+    list_display = ['key', 'value', '_file',]
+    ordering = ('key',)
+
 
