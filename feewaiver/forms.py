7816f0ba062b1898950eac33cd2d93acc73fb3db
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-01-08 14:25:20 +0800

    More code cleanup

--- feewaiver/forms.py
+++ feewaiver/forms.py
@@ -1,11 +1,5 @@
-#from __future__ import unicode_literals
-#from crispy_forms.bootstrap import FormActions
-#from crispy_forms.helper import FormHelper
-#from crispy_forms.layout import Layout, Submit
 from django.contrib.auth import get_user_model
-#from django.forms import Form, ModelForm, CharField, ValidationError, EmailField
 from django import forms
-#from ledger.accounts.models import Profile, Address, Organisation
 from feewaiver.main_models import SystemMaintenance
 import pytz
 from django.conf import settings
