b2f23756fed644157c265c06862754cdf8038765
Brendan Blackford <brendan.blackford@dbca.wa.gov.au>
2021-01-08 10:50:42 +0800

    Add EmailUser admin form override

--- feewaiver/admin.py
+++ feewaiver/admin.py
@@ -1,6 +1,47 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.contrib.auth.admin import UserAdmin
 from feewaiver.models import *
 from feewaiver.main_models import GlobalSettings
+from ledger.accounts import admin as ledger_admin
+from ledger.accounts.models import EmailUser
+from copy import deepcopy
+
+
+class FeeWaiverAdminSite(AdminSite):
+    site_header = 'Fee Waiver Administration'
+    site_title = 'Fee Waiver System'
+    index_title = 'Fee Waiver System'
+
+#feewaiver_admin_site = FeeWaiverAdminSite(name='feewaiveradmin')
+
+admin.site.unregister(EmailUser) # because this base classAdmin alsready registered in ledger.accounts.admin
+@admin.register(EmailUser)
+class EmailUserAdmin(ledger_admin.EmailUserAdmin):
+    """
+    Overriding the EmailUserAdmin from ledger.accounts.admin, to remove is_superuser checkbox field on Admin page
+    """
+
+    def get_fieldsets(self, request, obj=None):
+        """ Remove the is_superuser checkbox from the Admin page, if user is CommercialOperatorAdmin and NOT superuser """
+        fieldsets = super(UserAdmin, self).get_fieldsets(request, obj)
+        #if not obj:
+        #    return fieldsets
+
+        if request.user.is_superuser:
+            return fieldsets
+
+        # User is not a superuser, remove is_superuser checkbox
+        fieldsets = deepcopy(fieldsets)
+        for fieldset in fieldsets:
+            if 'is_superuser' in fieldset[1]['fields']:
+                if type(fieldset[1]['fields']) == tuple :
+                    fieldset[1]['fields'] = list(fieldset[1]['fields'])
+                fieldset[1]['fields'].remove('is_superuser')
+                break
+
+        return fieldsets
+
 
 @admin.register(FeeWaiver)
 class FeeWaiverAdmin(admin.ModelAdmin):
